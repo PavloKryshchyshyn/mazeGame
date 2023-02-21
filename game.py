20de7c3a8e1b43aedf96bd0760c9a5f09e9d4a15
Pablo <inshe.imia@gmail.com>
2023-02-21 20:14:41 +0200

    MORE COOOOOODE

--- game.py
+++ game.py
@@ -1,2 +1,14 @@
-def TreasureHunt():
+def treasureHunt():
+    print("\"Пошук Скарбів\"")
     name = input("введіть ім'я гравця: ")
+    choosenDifficulty = input("виберіть складність: \n1 легко, \n2 середньо, \n3 складно \n(вводьте лише цифри)\n")
+    waterCount = 2
+    movements = 5
+    dii = ["go", "dig", "search"]
+    def stats(self):
+        if self.choosenDifficulty == 1:
+            waterCount = 2
+            beastCount = 0
+            keyCount = 0
+            showelCount = 0
+            
